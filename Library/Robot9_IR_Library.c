a4cff00abf41c07cd488d95996bd5107cce40b30
Brian Yarbrough <C16Brian.Yarbrough@usafa.edu>
2014-12-04 09:04:33 -0700

    added library

---
+++ Library/Robot9_IR_Library.c
@@ -0,0 +1,10 @@
+/*
+ * Robot9_IR_Library.c
+ *
+ *  Created on: Dec 4, 2014
+ *      Author: C16Brian.Yarbrough
+ */
+
+
+
+
---
+++ Library/Robot9_IR_Library.h
@@ -0,0 +1,15 @@
+/*
+ * Robot9_IR_Library.h
+ *
+ *  Created on: Dec 4, 2014
+ *      Author: C16Brian.Yarbrough
+ */
+
+#ifndef ROBOT9_IR_LIBRARY_H_
+#define ROBOT9_IR_LIBRARY_H_
+
+
+
+
+
+#endif /* ROBOT9_IR_LIBRARY_H_ */
